0caf539b0334a440ad31d5264bdf5107921ac71e
Ubuntu <Nutsuda@vmparasite.t5cpl5zbzrhujl5jujrla30hmd.ix.internal.cloudapp.net>
2022-01-17 17:12:24 +0000

    basic argparse

--- python_script_101.py
+++ python_script_101.py
@@ -1,5 +1,34 @@
+import argparse #สำหรับ รับ input จ ากภายนอก
+import subprocess #สำหรับ รัน terminal command
+
+#import flask #สำหรับทำ web app และ webservice API
+
+
 def print_other():
     print('something else')
 
 if __name__ == "__main__":
-    print('test main funcion')
+    parser = argparse.ArgumentParser(description='test program to learn about argparse and subprocess')
+    parser.add_argument(
+        'm',
+        type=int,
+        help='value of m positional argument')
+                        
+    parser.add_argument(
+        '--x',
+        type=int,
+        help='value of x')
+
+    parser.add_argument(
+        '--yval',
+        type=int,
+        default=3,
+        help='value of y')
+                                
+    args = parser.parse_args()
+
+    x = args.x
+    y = args.yval
+
+    print(f'M = {args.m}')
+    print(f'calculate {x} x {y} = {x*y}')
